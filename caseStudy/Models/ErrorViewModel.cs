fd2c70f282ec5d9202c0d2a6ce026d586204e736
Agnes <agnes.h.rhee@gmail.com>
2026-07-08 18:11:18 -0400

    new proj setup

---
+++ caseStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace caseStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
